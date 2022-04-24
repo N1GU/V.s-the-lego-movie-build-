387ef2091c4889a0f60b7bc790c5d26481415586
Aaron Rasmus <66130669+voxovistired@users.noreply.github.com>
2022-04-24 06:34:35 -0500

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -415,11 +415,9 @@ class TitleState extends MusicBeatState
 
 				FlxG.camera.flash(FlxColor.WHITE, 1);
 				FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
-				//FlxTween.tween(titleText, { y: titleText.y + 700, x: titleText.x + 700}, 1, { type: FlxTween.PINGPONG, ease: FlxEase.quadInOut});
-				FlxTween.angle(titleText, 0, 90, 1.0, {ease: FlxEase.quadInOut, type: FlxTween.PINGPONG,});
+				FlxTween.tween(titleText, { y: titleText.y + 700, x: titleText.x + 700}, 1, { type: FlxTween.PINGPONG, ease: FlxEase.quadInOut});
 				FlxTween.tween(logoBl, { x: logoBl.x - 700}, 1, { type: FlxTween.PINGPONG, ease: FlxEase.quadInOut});
-				//FlxTween.tween(gfDance, { x: gfDance.x + 700 }, 1, { type: FlxTween.PINGPONG, ease: FlxEase.quadInOut});
-				FlxTween.angle(gfDance, 0, 90, 1.0, {ease: FlxEase.quadInOut, type: FlxTween.PINGPONG,});
+				FlxTween.tween(gfDance, { x: gfDance.x + 700 }, 1, { type: FlxTween.PINGPONG, ease: FlxEase.quadInOut});
 				transitioning = true;
 				// FlxG.sound.music.stop();
 
